d1e3b9e728266b53ab8b992e212610a970df28c8
Waldir Marquez POD <waldir.marquez@hirewithpod.com>
2023-09-19 20:02:13 -0500

    latest advance of the project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+  <footer className='bg-black py-8 text-center text-white'>
+    <div className='container mx-auto'>
+      <p>Copyright &copy; 2023. All rights reserved.</p>
+      <p>Made with ♥ by, <Link to='https://www.linkedin.com/in/waldir-marquez-espiritu/' target='__blanked'>Waldir Marquez Espiritu.</Link></p>
+    </div>
+  </footer>
+  );
 };
 
 export default Footer;
